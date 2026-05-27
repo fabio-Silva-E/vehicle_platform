cba8d99a89746be4f3723bdc1a10646bfecf0bd7
fabio-Silva-E <fs2894488@gmail.com>
2026-05-26 23:35:52 -0300

    debug True end new host site

--- vehicle_platform/settings/prod.py
+++ vehicle_platform/settings/prod.py
@@ -11,13 +11,13 @@ ALLOWED_HOSTS =  ["127.0.0.1", "localhost","terrier-equipped-supposedly.ngrok-fr
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ['POSTGRES_DB'],
-        'USER': os.environ['POSTGRES_USER'],
-        'PASSWORD': os.environ['POSTGRES_PASSWORD'],
-        'HOST': os.environ['POSTGRES_HOST'],
-        'PORT': int(os.environ['POSTGRES_DB_PORT']),
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': os.environ['POSTGRES_DB'],
+#        'USER': os.environ['POSTGRES_USER'],
+#        'PASSWORD': os.environ['POSTGRES_PASSWORD'],
+#        'HOST': os.environ['POSTGRES_HOST'],
+#        'PORT': int(os.environ['POSTGRES_DB_PORT']),
+#    }
+#}
