ed09e45df3c3775b79025191d937852458811a05
fabio-Silva-E <fs2894488@gmail.com>
2026-05-30 18:12:44 -0300

    updates and general adjustments in the system

--- vehicle_platform/settings/prod.py
+++ vehicle_platform/settings/prod.py
@@ -1,5 +1,5 @@
 # SECURITY WARNING: don't run with debug turned on in production!
-# import dj_database_url
+import dj_database_url
 from dotenv import load_dotenv
 
 from .base import *
@@ -14,22 +14,20 @@ ALLOWED_HOSTS = [
     "terrier-equipped-supposedly.ngrok-free.app",
     "vehicle-platform.onrender.com",
 ]
-# DATABASES = {
-#               'default': dj_database_url.parse(
-#                   os.getenv("DATABASE_URL")
-#               )
-#           }
-#
-#
-# Database
-# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ["POSTGRES_DB"],
-        "USER": os.environ["POSTGRES_USER"],
-        "PASSWORD": os.environ["POSTGRES_PASSWORD"],
-        "HOST": os.environ["POSTGRES_HOST"],
-        "PORT": int(os.environ["POSTGRES_DB_PORT"]),
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if DATABASE_URL:
+    DATABASES = {
+        "default": dj_database_url.parse(DATABASE_URL)
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.getenv("POSTGRES_DB"),
+            "USER": os.getenv("POSTGRES_USER"),
+            "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+            "HOST": os.getenv("POSTGRES_HOST"),
+            "PORT": int(os.getenv("POSTGRES_DB_PORT", 5432)),
+        }
+    }
